b9a2ba4085a7284d363825f3c4e1d3a719b118b5
Julia <juliasoaresdealmeidaj@gmail.com>
2023-03-31 09:48:37 -0300

    Update endpoint de alunos

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use((request, response, next) => {
 //EndPoints
 
 //EndPoint da função getNomeCursos que lista os cursos
-app.get('/v1/lion-school/cursos', cors(), async function(request, response, next) {
+app.get('/v1/lion-school/cursos', cors(), async function (request, response, next) {
     let cursos = jsonDados.getNomeCursos()
     let statusCode
     let dadosCurso = {}
@@ -50,17 +50,45 @@ app.get('/v1/lion-school/cursos', cors(), async function(request, response, next
 })
 
 //EndPoint da função getListaAlunos que lista todos os alunos da escola
-app.get('/v1/lion-school/alunos', cors(), async function(request, response, next) {
-    let alunos = jsonDados.getListaAlunos()
+app.get('/v1/lion-school/alunos', cors(), async function (request, response, next) {
+    // let alunos = jsonDados.getListaAlunos()
+    let getFuncao
+    let curso = request.query.curso
+    let status = request.query.status
     let statusCode
     let dadosAlunos = {}
 
-    if (alunos) {
-        statusCode = 200
-        dadosAlunos = alunos
+    if (status != undefined && curso == undefined) {
+
+        if (isNaN(status)) {
+            getFuncao = jsonDados.getStatusAlunos(status)
+            statusCode = 200
+            dadosAlunos = getFuncao
+        } else {
+            statusCode = 404
+        }
+
+    } else if (curso != undefined && status == undefined) {
+
+        if (isNaN(curso)) {
+            getFuncao = jsonDados.getAlunosCurso(curso)
+            statusCode = 200
+            dadosAlunos = getFuncao
+        } else {
+            statusCode = 404
+
+        }
     } else {
-        statusCode = 404
+        getFuncao = jsonDados.getListaAlunos()
+
+        if (getFuncao) {
+            statusCode = 200
+            dadosAlunos = getFuncao
+        } else {
+            statusCode = 404
+        }
     }
+
 
     response.status(statusCode)
     response.json(dadosAlunos)
@@ -69,7 +97,7 @@ app.get('/v1/lion-school/alunos', cors(), async function(request, response, next
 
 
 //EndPoint da função getMatriculaAlunos que traz os dados do aluno com base no número da matricula
-app.get('/v1/lion-school/alunos/:numeroMatricula', cors(), async function(request, response, next) {
+app.get('/v1/lion-school/alunos/:numeroMatricula', cors(), async function (request, response, next) {
     let numeroMatricula = request.params.numeroMatricula
     let statusCode
     let dadosMatricula = {}
@@ -94,62 +122,61 @@ app.get('/v1/lion-school/alunos/:numeroMatricula', cors(), async function(reques
 })
 
 //EndPoint da função getAlunosCurso que filtra os alunos de acordo com seu curso
-app.get('/v1/lion-school/alunoss', cors(), async function(request, response, next) {
-    let siglaCurso = request.query.curso
-    let statusCode
-    let dadosCursoSigla = {}
+// app.get('/v1/lion-school/alunoss', cors(), async function (request, response, next) {
+//     let siglaCurso = request.query.curso
+//     let statusCode
+//     let dadosCursoSigla = {}
 
 
-    if (siglaCurso == '' || siglaCurso == undefined || !isNaN(siglaCurso)) {
+//     if (siglaCurso == '' || siglaCurso == undefined || !isNaN(siglaCurso)) {
 
-        statusCode = 400
-        dadosCursoSigla.message = 'Não foi possível processar pois os dados de entrada que foi enviado não corresponde ao exigido.'
+//         statusCode = 400
+//         dadosCursoSigla.message = 'Não foi possível processar pois os dados de entrada que foi enviado não corresponde ao exigido.'
 
-    } else {
-        let cursoSigla = jsonDados.getAlunosCurso(siglaCurso)
+//     } else {
+//         let cursoSigla = jsonDados.getAlunosCurso(siglaCurso)
 
-        if (cursoSigla) {
-            statusCode = 200
-            dadosCursoSigla = cursoSigla
-        } else {
-            statusCode = 404
-        }
-    }
+//         if (cursoSigla) {
+//             statusCode = 200
+//             dadosCursoSigla = cursoSigla
+//         } else {
+//             statusCode = 404
+//         }
+//     }
 
-    response.status(statusCode)
-    response.json(dadosCursoSigla)
+//     response.status(statusCode)
+//     response.json(dadosCursoSigla)
 
-})
+// })
 
 //EndPoint da função getStatusAluno que filtra o status do aluno
-app.get('/v1/lion-school/alunosss', cors(), async function(request, response, next) {
-    let statusAluno = request.query.status
-    let statusCode
-    let dadosAlunoStatus = {}
+// app.get('/v1/lion-school/alunosss', cors(), async function (request, response, next) {
+//     let statusAluno = request.query.status
+//     let statusCode
+//     let dadosAlunoStatus = {}
 
-    if (statusAluno == '' || statusAluno == undefined || !isNaN(statusAluno)) {
-        statusCode = 400
-        dadosAlunoStatus.message = 'Não foi possível processar pois os dados de entrada que foi enviado não corresponde ao exigido.'
+//     if (statusAluno == '' || statusAluno == undefined || !isNaN(statusAluno)) {
+//         statusCode = 400
+//         dadosAlunoStatus.message = 'Não foi possível processar pois os dados de entrada que foi enviado não corresponde ao exigido.'
 
-    } else {
-        let status = jsonDados.getStatusAlunos(statusAluno)
+//     } else {
+//         let status = jsonDados.getStatusAlunos(statusAluno)
 
-        if (status) {
-            statusCode = 200
-            dadosAlunoStatus = status
-        } else {
-            statusCode = 404
-        }
-    }
+//         if (status) {
+//             statusCode = 200
+//             dadosAlunoStatus = status
+//         } else {
+//             statusCode = 404
+//         }
+//     }
 
-    response.status(statusCode)
-    response.json(dadosAlunoStatus)
-
-
-})
+//     response.status(statusCode)
+//     response.json(dadosAlunoStatus)
 
 
+// })
 
-app.listen(8080, function() {
+
+app.listen(8080, function () {
     console.log('Servidor aguardando requisições na porta 8080.')
 })
